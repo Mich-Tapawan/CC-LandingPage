f001c9907d6687106eb8a903b55e0ed93a803e21
Mich-Tapawan <mik.tapawan@gmail.com>
2024-05-05 23:08:35 +0800

    Created a navbar background color scrolling effect

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const nav = document.getElementsByTagName('nav');
+
+    // Navigation Bar Scrolling Effect
+    var navbar = document.getElementById('fixed-nav');
+
+    window.addEventListener('scroll', ()=>{
+        if(window.scrollY > 0){
+            navbar.style.backgroundColor = '#fff';
+            navbar.style.marginTop = '0';
+        }
+        else{
+            navbar.style.backgroundColor = 'transparent';
+            navbar.style.paddingTop = '10px';
+        }
+    })
 });
