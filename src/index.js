363d3ac3de75e1299f6191dec211860a2481ce31
Mich-Tapawan <mik.tapawan@gmail.com>
2024-05-19 23:24:18 +0800

    Added moving effect when scrolling for navbar content

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     // Navbar - Scrolling Effect
     var navbar = document.getElementById('fixed-nav');
+    var navLogo = document.querySelector('#nav-logo');
 
     window.addEventListener('scroll', ()=>{
         if(window.scrollY > 0){
             navbar.style.backgroundColor = '#fff';
             navbar.style.boxShadow = '0px 0.5px 8px 1px #858585';
+            navbar.style.paddingBottom = '10px';
+            navLogo.style.paddingBottom = '5px';
         }
         else{
             navbar.style.backgroundColor = 'transparent';
             navbar.style.paddingTop = '10px';
             navbar.style.boxShadow = '0px 1px 2px 1px transparent';
+            navbar.style.paddingBottom = '0px'
+            navLogo.style.paddingBottom = '0px';
         }
     });
 
